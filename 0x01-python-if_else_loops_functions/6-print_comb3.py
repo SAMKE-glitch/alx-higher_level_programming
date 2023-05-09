838901ed05a93569fa57a5c27c77db9b246ae7f8
SAMKE-glitch <sammythemwa@gmail.com>
2023-05-09 10:17:08 -0700

    printing combination of 2 numbers

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for k in range(i + 1, 10):
+        if i == 8 and k == 9:
+            print("{}{}".format(i, k))
+        else:
+            print("{}{}".format(i, k), end=", ")
