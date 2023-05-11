9e5bbe3ea56b948868fe54f343f969b4fe769156
SAMKE-glitch <sammythemwa@gmail.com>
2023-05-11 08:38:21 -0700

    made some changes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """printing the number of and list of arguments"""
     import sys
-    
+
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
@@ -12,5 +11,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(count))
     for i in range(count):
-        print("{}:{}".format(i + 1, sys.argv[i + 1]))
-
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
