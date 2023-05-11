11cbf753f00efb12b1bc53812e75cc5ec3b9e590
SAMKE-glitch <sammythemwa@gmail.com>
2023-05-11 06:36:06 -0700

    made some changes

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-def add(a, b):
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, a + b))
-    print()
+from add_0 import add
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
