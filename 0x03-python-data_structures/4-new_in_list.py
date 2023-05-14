2b758af7328dbc00bd9c2f53911e3a79c169ee20
SAMKE-glitch <sammythemwa@gmail.com>
2023-05-14 04:14:16 -0700

    replace with a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,5 +6,5 @@ def new_in_list(my_list, idx, element):
             return my_list
         else:
             new_list = my_list.copy()
-            my_list[idx] = element
+            new_list[idx] = element
             return new_list
