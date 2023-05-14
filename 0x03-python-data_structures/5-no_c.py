b281331aa3aa616baa2843f5fccf04dc813eadfe
SAMKE-glitch <sammythemwa@gmail.com>
2023-05-14 04:27:30 -0700

    returning string wito no C,c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    if my_string:
-        updated_string = ''
-        for i in my_string:
-            if i != 'c' and i != 'C':
-                updated_string += i
-        return (updated_string)
+    updated_string = ''
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            updated_string += i
+    return (updated_string)
 
