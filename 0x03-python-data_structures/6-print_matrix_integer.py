e9aebb668d230bcbee9018271a59d033439cc7f7
SAMKE-glitch <sammythemwa@gmail.com>
2023-05-15 01:59:09 -0700

    printing integer matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
-            print(element, end=' ')
+            print("{:d}".format(element), end=' ')
         print()
