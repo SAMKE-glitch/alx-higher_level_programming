bb9ddc38ecf8f4f3bf30cf6e0966da93b8092f66
SAMKE-glitch <sammythemwa@gmail.com>
2023-05-15 05:27:24 -0700

    mulitple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    for i in sentence:
-        return len(sentence), sentence[0]
+    if sentence != '':
+        return (len(sentence), sentence[0])
+    else:
+        return (len(sentence, None))
