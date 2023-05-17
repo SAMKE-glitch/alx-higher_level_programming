6fbd11ee9e86e8bf8878bc6bfc212eef9d68c44a
SAMKE-glitch <sammythemwa@gmail.com>
2023-05-17 11:14:33 -0700

    roman to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+    results = 0
+    roman_values = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    for char in reversed(roman_string):
+        value = roman_values[char]
+        results += value if results < value * 5 else -value
+    return results
