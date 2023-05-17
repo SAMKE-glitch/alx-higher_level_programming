b194be0b950e573d14849f05d7b66c78a17d3e40
SAMKE-glitch <sammythemwa@gmail.com>
2023-05-17 08:18:17 -0700

    replacing items

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    if my_list:
-        new_list = []
-        for i in my_list:
-            if i == search:
-                new_list.append(replace)
-            else:
-                new_list.append(i)
-        return new_list
+    def find_search(i):
+        return i if i != search else replace
+    return list(map(find_search, my_list))
