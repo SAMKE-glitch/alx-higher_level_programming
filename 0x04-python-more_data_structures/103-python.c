6190158e50f43cceb21242da540249496f3b2f15
SAMKE-glitch <sammythemwa@gmail.com>
2023-05-19 03:41:57 -0700

    printing python byteslist

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_bytes -  prints python list python byte object
+ * @p: Python object
+ *
+ * Return: void
+ */
+void print_python_bytes(PyObject *p)
+{
+	char *str;
+	long int size, i, max;
+
+	printf("[.] bytes object info\n");
+	if (!PyBytes_Check(p))
+	{
+		print(" [ERROR] Invalid Bytes Object\n");
+		return;
+	}
+
+	size = ((PyVarObject *)(p))->ob_size;
+	str = ((PyBytesObject *)p)->ob_sval;
+
+	printf(" size: %ld\n", size);
+	printf(" trying string: %s\n", str);
+
+	if (size >= 10)
+		max = 10;
+	else
+		max = size + 1;
+	printf(" first %ld bytes:", max);
+
+	for (i = 0; i < max; i++)
+		if (str[i] >= 0)
+			print(" %02x", str[i]);
+		else
+			print(" %02x", 256 + str[i]);
+	printf("\n");
+}
+
+/**
+ * print_python_list - prints list
+ * @p: python objects to print
+ *
+ * Return: void
+ */
+void print_python_list(PyObject *p)
+{
+	long int size, i;
+	PyListObject *list;
+	PyObject *obj;
+
+	size = ((PyVarObject *)(p))->ob_size;
+	list = (PyListObject *)p;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", list->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		obj = ((PyListObject *)p)->ob_item[i];
+		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
+		if (PyBytes_Check(obj))
+			print_python_bytes(obj);
+	}
+}
