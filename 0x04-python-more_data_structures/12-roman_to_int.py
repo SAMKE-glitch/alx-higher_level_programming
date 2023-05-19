201bcd634095b67ae2b0257268bb29e5a1bf61a6
SAMKE-glitch <sammythemwa@gmail.com>
2023-05-19 01:09:41 -0700

    checked the pycodestyle

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
         return 0
     results = 0
     roman_values = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10,
-            'V': 5, 'I': 1}
+                        'V': 5, 'I': 1}
     for char in reversed(roman_string):
         value = roman_values[char]
         results += value if results < value * 5 else -value
