07e800e99cf660ad5c81f60b6403c7f0b96d7931
SAMKE-glitch <sammythemwa@gmail.com>
2023-05-30 09:41:48 -0700

    size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""module that defines a square """
+
+class Square:
+    """ A class that represents a square """
+
+    def __init__(self, size=0):
+        """ initialzing the square
+        Args:
+            size: represents the size of the square defined
+        Raises:
+            TypError: if the size isn't integer
+            VelueError: if size is less than zero
+        """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
