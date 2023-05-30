028c8004e1d2608cbb86f7b63e1c0ee83a625fb9
SAMKE-glitch <sammythemwa@gmail.com>
2023-05-30 09:29:35 -0700

    initializing size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Defines a square """
+
+
+class Square:
+    """Representing a square """
+
+    def __init__(self, size):
+        """ Initializing square class
+        args: size represents the size of the square"""
+
+        self.__size = size
