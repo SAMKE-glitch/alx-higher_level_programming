f30c42786aa76b9fb046f4ac5e3ac186fd93c5d6
SAMKE-glitch <sammythemwa@gmail.com>
2023-06-12 10:14:19 -0700

    a subclass square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a rectangle subclass square"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initializes a new square"""
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
