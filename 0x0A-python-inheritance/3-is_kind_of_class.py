4e95dfbda68226c233a423c2f1870159539dbb0e
SAMKE-glitch <sammythemwa@gmail.com>
2023-06-12 07:19:16 -0700

    if object is an instance of the inherited class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    A module that checks if object is
+    an instance of a class defined or inherited
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+        function or method that checks if object is an
+        instance of a class that inherited from
+    """
+    return (isinstance(obj, a_class))
