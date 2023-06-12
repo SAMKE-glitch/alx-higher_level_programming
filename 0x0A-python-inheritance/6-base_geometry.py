3510f4143040f3fd3da4b41afac8ebca9707a8c3
SAMKE-glitch <sammythemwa@gmail.com>
2023-06-12 07:58:16 -0700

    a class that defines unimplemented area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    A module that defines a class
+"""
+
+
+class BaseGeometry:
+    """
+        Class that represents Base geometry
+    """
+    def area(self):
+        """
+            Method that has not been implemented
+        """
+        raise Exception("area() is not implemented")
