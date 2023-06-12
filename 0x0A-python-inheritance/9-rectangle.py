0dc25d44d9821a442878ce7f6ed7f40167142ce6
SAMKE-glitch <sammythemwa@gmail.com>
2023-06-12 09:57:46 -0700

    full rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A module that defines a class Rectangle that inherits from BaseGeometry"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Class represents a rectangle using BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Initializesa new rectangle"""
+
+        super().integer_validator("width", height)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """returns the print() and str() representation of a Rectangle"""
+        string = "[" + str(self.__class__.__name__) + "]"
+        string += str(self.__width) + "/" + str(self.__height)
+        return string
