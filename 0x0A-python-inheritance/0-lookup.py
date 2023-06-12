d593482b592e93f86447e31009742690bd302a3a
SAMKE-glitch <sammythemwa@gmail.com>
2023-06-12 06:14:55 -0700

    lookup attributes and methods in obj

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    Module that has a function that returns available
+    attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """Methods that returns attributes and methods of obj"""
+    return dir(obj)
