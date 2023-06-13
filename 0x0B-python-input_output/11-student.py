e4f12177dacce62934496e21c8e07ac7d2bb48d2
SAMKE-glitch <sammythemwa@gmail.com>
2023-06-13 13:45:25 -0700

    student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module defines a class Stduent"""
+
+
+class Stduent:
+    """Class representing a stduent"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new studnet"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Gets a dictionary representation of the student.
+        if attrs is a list of strings, represents only those attributes
+        included in the list
+        """
+        if (type(attrs) == list and all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the student"""
+        for k, v in json.items():
+            setattr(self, k, v)
