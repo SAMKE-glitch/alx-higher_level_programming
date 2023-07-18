bde1bea5478ca4add811e0328f84ad4adc8a2264
SAMKE-glitch <sammythemwa@gmail.com>
2023-07-18 08:12:10 -0700

    created a table

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,2 @@
+-- script that lists  all the tables of my database in MYSQL
+SHOW TABLES;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- scripts that creates a table called first_table 
+-- in the current database in MYSQL Server
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
