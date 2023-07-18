7007f109abd10749a8ae9bef22238b1dce705fdf
SAMKE-glitch <sammythemwa@gmail.com>
2023-07-18 09:33:09 -0700

    added files

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,6 @@
 -- displays the avergafe temp by city ordered 
 -- by temperature desc
 SELECT city, AVG(value) as avg_temp
-FROM temnperatures
+FROM temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -4,5 +4,5 @@ SELECT city, AVG(value) as avg_temp
 FROM temperatures
 WHERE month = 7 OR month = 8
 GROUP BY city
-ORDER BY avg_temp DSC
+ORDER BY avg_temp DESC
 LIMIT 3;
