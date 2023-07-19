496d0e0e95032dd2b588507d9587c5dc812130cc
SAMKE-glitch <sammythemwa@gmail.com>
2023-07-19 08:01:19 -0700

    fixed task 5

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -6,4 +6,5 @@
 -- If the table unique_id already exists, your script should not fail
 CREATE TABLE IF NOT EXISTS 'unique_id'(
 'id' INT DEFAULT 1 UNIQUE,
-'name' VARCHAR(256));
+'name' VARCHAR(256)
+);
