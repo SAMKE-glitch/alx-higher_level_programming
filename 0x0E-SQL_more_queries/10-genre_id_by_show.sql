7dc21fead1ba03f1f9e8cd7bcde9eb17fa227c3c
SAMKE-glitch <sammythemwa@gmail.com>
2023-07-19 08:41:24 -0700

    added 5 files

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -3,8 +3,8 @@
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT s.'title', g.'genre_id'
-FROM 'tv_shows' AS s
-INNER JOIN 'tv_show_genres' AS g
-ON s.'id' = g. 'genre_id'
-ORDER BY s.'title', g.'genre_id';
+SELECT s.`title`, g.`genre_id`
+FROM `tv_shows` AS s
+INNER JOIN `tv_show_genres` AS g
+ON s.`id` = g. `genre_id`
+ORDER BY s.`title`, g.`genre_id`;
