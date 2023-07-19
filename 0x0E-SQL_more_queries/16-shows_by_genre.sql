b12acf270f489ad74f36b1ad4bcfeeec8e535d4b
SAMKE-glitch <sammythemwa@gmail.com>
2023-07-19 09:22:41 -0700

    files scripts

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -9,6 +9,6 @@ FROM `tv_shows` AS t
 LEFT JOIN `tv_show_genres` AS s
 ON t.`id` = s.`show_id`
 
-LEFT JOIN `tv_show_genres` AS s
+LEFT JOIN `tv_show_genres` AS g
 ON s.`genre_id` = g.`id`
 ORDER BY t.`title`, g.`name`;
