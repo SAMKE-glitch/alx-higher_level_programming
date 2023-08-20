15bf387c3f9b43e977a1fd67d2de4158d244d34f
SAMKE-glitch <sammythemwa@gmail.com>
2023-08-20 01:19:37 -0700

    getiing and printing states from the db

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
     """
     Acess to the database and get the states from the database
     """
-    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306, passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
