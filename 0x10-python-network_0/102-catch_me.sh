323ef8e645268ad5db630d2cf4d6e40d1dbecb99
SAMKE-glitch <sammythemwa@gmail.com>
2023-08-31 06:22:20 -0700

    catch me if you can

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -sL -X PUT "Origin: ALX" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "Origin: ALX" -d "user_id=98" 0.0.0.0:5000/catch_me
