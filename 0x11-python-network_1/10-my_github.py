c5ba3ac84d26018dafc3cd93c0e1e3c721f423af
SAMKE-glitch <sammythemwa@gmail.com>
2023-09-03 10:27:52 +0300

    my github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,6 +11,25 @@ from requests.auth import HTTPBasicAuth
 
 
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get("https://github.com/user", auth=auth)
-    print(r.json().get("id"))
+    if len(sys.argv) != 3:
+        print("Usage: ./10-my_github.py <Github username> <Github password>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    # Construct the API URL to get user information
+    url = "https://api.github.com/user"
+
+    # Send a GET request with Basic Authentication
+    auth = HTTPBasicAuth(username, password)
+    response = requests.get(url, auth=auth)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        user_data = response.json()
+        user_id = user_data.get("id")
+        print("Your GitHub user ID is:", user_id)
+    else:
+        print("Failed to retrieve user data. Please check your credentials")
+        sys.exit(1)
