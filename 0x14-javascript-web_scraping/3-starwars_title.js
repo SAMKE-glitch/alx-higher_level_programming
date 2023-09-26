7d6c10476e52c752354e4e9f3f2a8b1359adadf1
SAMKE-glitch <sammythemwa@gmail.com>
2023-09-26 02:55:53 -0700

    made changes

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const movieId = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films/${movieId}';
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request.get(url, (error, response, body) => {
   if (error) {
